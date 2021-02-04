766036b8af53f3e1c941c7784a9333e20dbb5988
Vetlix <31412314+Vetlix@users.noreply.github.com>
2021-02-04 23:44:36 +0100

    fix: fuck typescript

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,4 @@ import { CentraRequest, HTTPMethod } from './lib/CentraRequest';
  * @param {(URL | string)} url
  * @param {HTTPMethod} [method='GET']
  */
-const req = (url: URL | string, method: HTTPMethod = 'GET') => new CentraRequest(url, method);
-
-export = req;
+export = (url: URL | string, method: HTTPMethod = 'GET') => new CentraRequest(url, method);;
