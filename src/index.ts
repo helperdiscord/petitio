9d5a9f4601c46dea934501327702bc2c8ee6d1f5
tbnritzdoge <shadowphoenixcoc@gmail.com>
2021-02-23 14:11:42 -0500

    feat: bundle and minify for speed

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ import { URL } from 'url';
  * @param {(URL | string)} url
  * @param {HTTPMethod} [method='GET']
  */
-export = (url: URL | string, method: HTTPMethod = 'GET') => new CentraRequest(url, method);;
+export = (url: URL | string, method: HTTPMethod = 'GET') => new CentraRequest(url, method);
