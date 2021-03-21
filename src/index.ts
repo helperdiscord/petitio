f4b7ec0a24f792c5efac1296ba7c84180d0ab561
Nytelife26 <xtylerjrx@gmail.com>
2021-03-21 22:24:12 +0000

    fix: export correctly

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,6 @@ import { URL } from "url";
  * @param {(URL | string)} url
  * @param {HTTPMethod} [method='GET']
  */
-export function petitio(url: URL | string, method: HTTPMethod = "GET") {
+export = function petitio(url: URL | string, method: HTTPMethod = "GET") {
 	return new PetitioRequest(url, method);
 }
-export default petitio;
