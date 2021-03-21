3dfcc2a150ac2745989acc699b0276815d6891d9
Nytelife26 <xtylerjrx@gmail.com>
2021-03-21 22:55:13 +0000

    ci: implement semantic-release

---
+++ release.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/changelog",
+		"@semantic-release/npm",
+		"@semantic-release/github",
+		[
+			"@semantic-release/git",
+			{
+				assets: [
+					"package.json",
+					"package-lock.json",
+					"CHANGELOG.md"
+				],
+				message: "chore: release ${nextRelease.version}\n\n${nextRelease.notes}"
+			}
+		]
+	]
+};
