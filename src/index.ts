c00404d6a494c249b14c96f9961a23c5a59e4cef
Nytelife26 <xtylerjrx@gmail.com>
2021-03-21 04:48:44 +0000

    feat: proper import mapping

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import { URL } from "url";
  * @param {(URL | string)} url
  * @param {HTTPMethod} [method='GET']
  */
-export = function petitio(url: URL | string, method: HTTPMethod = "GET") {
+export function petitio(url: URL | string, method: HTTPMethod = "GET") {
 	return new PetitioRequest(url, method);
 }
+export default petitio;
+if (module) module.exports = petitio;
