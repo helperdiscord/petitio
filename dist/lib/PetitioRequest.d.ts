303db35ac4cd3157db6a630d7b9430da2405968c
Kludge Cyber Systems (CI) <contact@kludge-cs.co.uk>
2021-03-22 21:34:15 +0000

    build: v2.0.0

---
+++ dist/lib/PetitioRequest.d.ts
@@ -0,0 +1,169 @@
+/**
+ * @module PetitioRequest
+ */
+/// <reference types="node" />
+import type ClientType from "undici/types/client";
+import type { IncomingHttpHeaders } from "http";
+import type { ParsedUrlQueryInput } from "querystring";
+import { PetitioResponse } from "./PetitioResponse";
+import { URL } from "url";
+/**
+ * Accepted HTTP methods (currently only supports up to HTTP/1.1).
+ */
+export declare type HTTPMethod = "GET" | "POST" | "PATCH" | "DELETE" | "PUT";
+/**
+ * @see [Undici ClientOptions timeout documentation](https://github.com/nodejs/undici/blob/main/docs/api/Client.md#parameter-clientoptions)
+ */
+export interface TimeoutOptions {
+    bodyTimeout?: number;
+    headersTimeout?: number;
+    keepAliveTimeout?: number;
+}
+export declare class PetitioRequest {
+    /**
+     * Options to use for Undici under the hood.
+     * @see [Undici ClientOptions documentation](https://github.com/nodejs/undici/blob/main/docs/api/Client.md#parameter-clientoptions)
+     */
+    coreOptions: ClientType.Options;
+    /**
+     * The data to be sent as the request body.
+     */
+    data?: string | Buffer;
+    /**
+     * @see [[HTTPMethod]]
+     */
+    httpMethod: HTTPMethod;
+    /**
+     * @see [[PetitioRequest.client]]
+     */
+    kClient?: ClientType;
+    /**
+     * Whether [[PetitioRequest.kClient]] will persist between [[PetitioRequest.send]]
+     * calls. It is recommended to enable this for superior performance.
+     */
+    keepClient?: boolean;
+    /**
+     * The headers to attach to the request.
+     */
+    reqHeaders: IncomingHttpHeaders;
+    /**
+     * The timeout options for the Undici client.
+     * @see [[TimeoutOptions]]
+     */
+    timeoutOptions: TimeoutOptions;
+    /**
+     * The URL destination for the request, targeted in [[PetitioRequest.send]].
+     */
+    url: URL;
+    /**
+     * @param {(string | URL)} url The URL to start composing a request for.
+     * @param {HTTPMethod} [httpMethod="GET"] The HTTP method to use.
+     * @return {PetitioRequest} The Petitio request instance for your URL.
+     */
+    constructor(url: string | URL, httpMethod?: HTTPMethod);
+    /**
+     * @param {ClientType} client The Undici client instance you wish to use.
+     * @param {boolean} keepAlive Whether to persist the client across requests or not.
+     * @return {*} The request object for further composition.
+     * @see [Undici Client documentation](https://github.com/nodejs/undici/blob/main/docs/api/Client.md)
+     */
+    client(client: ClientType, keepAlive?: boolean): this;
+    /**
+     * @param {*} key The query key to use for the URL query parameters.
+     * @param {*} value The value to set the query key to.
+     * @example
+     * If you wish to make a query at https://example.com/index?query=parameter
+     * you can use `.query("query", "parameter")`.
+     */
+    query(key: string, value: any): this;
+    /**
+     * @param {*} key An object of query keys and their respective values.
+     * @example
+     * If you wish to make multiple queries at once, you can use
+     * `.query({"keyOne": "hello", "keyTwo": "world!"})`.
+     */
+    query(key: Record<string, any>): this;
+    /**
+     * @param {string} relativePath A path to resolve relative to the current URL.
+     * @return {*} The request object for further composition.
+     * @example `https://example.org/hello/world` with `.path("../petitio")`
+     * would resolve to `https://example.org/hello/petitio`.
+     */
+    path(relativePath: string): this;
+    /**
+     * @param {*} data The data to be set for the request body.
+     */
+    body(data: Buffer | string): this;
+    /**
+     * @param {*} data The data to be set for the request body.
+     * @param {*} sendAs If data is set to any object type value other than a
+     * buffer or this is set to `json`, the `Content-Type` header will be set to
+     * `application/json` and the request data will be set to the stringified
+     * JSON form of the supplied data.
+     */
+    body(data: Record<string, any>, sendAs?: "json"): this;
+    /**
+     * @param {*} data The data to be set for the request body.
+     * @param {*} sendAs If data is a string or a parsed object of query
+     * parameters *AND* this is set to `form`, the `Content-Type` header will be
+     * set to `application/x-www-form-urlencoded` and the request data will be
+     * set to the URL encoded version of the query string.
+     */
+    body(data: ParsedUrlQueryInput | string, sendAs: "form"): this;
+    /**
+     * @param {*} header The encoded header name to set.
+     * @param {*} value The value to set the header to.
+     */
+    header(header: string, value: string): this;
+    /**
+     * @param {*} header An object of keys and values to set headers to.
+     */
+    header(header: Record<string, string>): this;
+    /**
+     * @param {*} method The HTTP method to change the request to.
+     * @return {*} The request object for further composition.
+     */
+    method(method: HTTPMethod): this;
+    /**
+     * @param {*} timeout The timeout (in milliseconds) to set the `bodyTimeout`
+     * to.
+     * @see [[TimeoutOptions.bodyTimeout]]
+     */
+    timeout(timeout: number): this;
+    /**
+     * @param {*} timeout The timeout option to change.
+     * @param {*} time The number of milliseconds to set the timeout to.
+     * @see [[TimeoutOptions]]
+     */
+    timeout(timeout: keyof TimeoutOptions, time: number): this;
+    /**
+     * @param {*} key An object of key-value options to set for Undici.
+     * @see [Undici Client documentation](https://github.com/nodejs/undici/blob/main/docs/api/Client.md)
+     */
+    option(key: ClientType.Options): this;
+    /**
+     * @template T
+     * @param {T} key The client options key to set.
+     * @param {ClientType.Options[T]} value The value to set the client option to (type checked).
+     * @see [Undici Client documentation](https://github.com/nodejs/undici/blob/main/docs/api/Client.md)
+     */
+    option<T extends keyof ClientType.Options>(key: T, value: ClientType.Options[T]): this;
+    /**
+     * @template T Type casting parameter for the JSON result.
+     * @return {*} A serialized object result from sending the request.
+     */
+    json<T = any>(): Promise<T>;
+    /**
+     * @return {*} The raw response body as a buffer.
+     */
+    raw(): Promise<Buffer>;
+    /**
+     * @return {*} The raw response body as a string.
+     */
+    text(): Promise<string>;
+    /**
+     * Finalizes and sends the composable request to the target server.
+     * @return {*} The response object.
+     */
+    send(): Promise<PetitioResponse>;
+}
---
+++ dist/lib/PetitioResponse.d.ts
@@ -0,0 +1,51 @@
+/**
+ * @module PetitioResponse
+ */
+/// <reference types="node" />
+export declare class PetitioResponse {
+    /**
+     * The response body received from the server.
+     * This is updated in chunks through [[PetitioResponse._addChunk]], either
+     * from [[PetitioRequest.send]] or directly on a response object from
+     * another source.
+     */
+    body: Buffer;
+    /**
+     * The response headers received from the server.
+     * This is updated through [[PetitioResponse._parseHeaders]].
+     */
+    headers: {
+        [k: string]: any;
+    };
+    /**
+     * The status code received from the server.
+     * This is set only after the response is complete when headers are received
+     * or it can be set manually.
+     */
+    statusCode: number | null;
+    /**
+     * This appends data to the body, dynamically reallocating the buffer size
+     * as chunks are added. Therefore, this is currently unsuitable for handling
+     * large responses, as the exact size is allocated in memory as a buffer.
+     * @param {*} chunk The chunk of data to append to the body.
+     * @return {*} In place operation with no return.
+     */
+    _addChunk(chunk: Buffer | Uint8Array): void;
+    /**
+     * @param {*} headers The headers to add. This is done by splitting the
+     * array into chunks of two, where the first value becomes the header and
+     * the latter becomes its value. This will also append values to the header
+     * as an array if it already exists.
+     * @return {*} In place operation with no return.
+     */
+    _parseHeaders(headers: string[]): void;
+    /**
+     * @template T Type casting parameter for the JSON result.
+     * @return {*} A serialized object result parsed from the response body.
+     */
+    json<T = any>(): T;
+    /**
+     * @return {*} The response body decoded as a UTF-8 string from the buffer.
+     */
+    text(): string;
+}
